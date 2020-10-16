2f7c4ddda4801815bc5f7e55f036c402d02fe49c
hayato.saishu <h3.sbskll9@gmail.com>
2020-10-16 13:13:41 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/ner'
+  get 'users/create'
   root to: 'tasks#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create, :new]
+  
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
